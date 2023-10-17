1a8fe895e57c3d44d3ec2b1a9e7b6667fda09c7a
Ivan <ragna.blood82@gmail.com>
2023-10-17 13:55:05 +0300

    Completed

--- Home_W_15.py
+++ Home_W_15.py
@@ -17,15 +17,53 @@ class Transport:
      self.mileage = mileage
 
 class Bus(Transport):
-
  def __init__(self, name, max_speed, mileage):
 
   super().__init__(name, max_speed, mileage)
 
-def __str__(self):
+ def __str__(self):
+     
+  return "Название автомобиля: "+self.name+" Скорость: "+str(self.max_speed)+" Пробег:  "+str(self.mileage)
 
- return "Название автомобиля: "+self.name+" Скорость: "+str(self.max_speed)+" Пробег:  "+str(self.mileage)
+bus = Bus("Renault Logan", 180, 12)
 
-bus=Bus("Renault Logan", 180, 12)
+print("такая вот тачка: " + bus)
 
-print(bus)
+
+
+class Transport:
+
+   def __init__(self, name, max_speed, mileage):
+
+    self.name = name
+    self.max_speed = max_speed
+    self.mileage = mileage
+ 
+
+   def seating_capacity(self, capacity):
+
+       return f"Вместимость одного автобуса {self.name}  {capacity} пассажиров"
+   
+   class Transport:
+
+    def __init__(self, name, max_speed, mileage):
+
+     self.name = name
+
+     self.max_speed = max_speed
+
+     self.mileage = mileage
+
+def seating_capacity(self, capacity):
+
+ return f"Вместимость одного автобуса {self.name} {capacity} пассажиров"
+
+class Autobus(Transport):
+
+ def seating_capacity(self, capacity = 50):
+
+  return super ().seating_capacity (capacity = 50)
+
+School_bus = Autobus("School Volvo", 180, 12)
+
+print(School_bus.seating_capacity())
